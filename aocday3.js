d779dd8424e4c96ef43ee821ba1d30623e1ed7e4
zachatrocity <zachatrocity@gmail.com>
2015-12-03 11:27:41 -0600

    no message

---
+++ aocday3.js
@@ -0,0 +1,39 @@
+//var input = document.getElementsByTagName("pre")[0].innerText;
+
+var input =  "^>v<";
+
+function node(count) {
+	this.visitCount = count;
+}
+
+//santa visits first house
+var block = [[new node(1)]];
+
+
+void calcHouses = function(dir){
+	switch(expression) {
+	    case '^':
+	        block.unshift()
+	        break;
+	    case '>':
+	        //code block
+	        break;
+        case 'v':
+	        //code block
+	        break;
+        case '<':
+	        //code block
+	        break;
+	    default:
+	        default code block
+	}
+}
+
+var housesVisited = 0;
+
+for (var i = 0; i < input.length; i++) {
+	calcHouse(input[i]);
+};
+
+
+
