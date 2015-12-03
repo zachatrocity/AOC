afe19cae12b8699bae63ac6309e54fe9e8e79e36
zachatrocity <zachatrocity@gmail.com>
2015-12-03 17:24:11 -0600

    no message

--- aocday3.js
+++ aocday3.js
@@ -1,39 +1,39 @@
-//var input = document.getElementsByTagName("pre")[0].innerText;
-
-var input =  "^>v<";
-
-function node(count) {
-	this.visitCount = count;
-}
+var input = document.getElementsByTagName("pre")[0].innerText;
 
 //santa visits first house
-var block = [[new node(1)]];
+var block = {}; block[[0,0]] = 1;
+var santa = {x: 0, y:0} //x, y
 
-
-void calcHouses = function(dir){
-	switch(expression) {
-	    case '^':
-	        block.unshift()
-	        break;
-	    case '>':
-	        //code block
-	        break;
-        case 'v':
-	        //code block
-	        break;
-        case '<':
-	        //code block
-	        break;
-	    default:
-	        default code block
-	}
+var calcHouses = function(dir, person){
+    if(dir == '^' && !([person.x, ++person.y] in block)){
+    	block[[person.x, person.y]] = 1;
+    } else if(dir == '>' && !([++person.x, person.y] in block)){
+    	block[[person.x, person.y]] = 1;
+    } else if(dir == 'v' && !([person.x, --person.y] in block)){
+    	block[[person.x, person.y]] = 1;
+    } else if (dir == '<' && !([--person.x, person.y] in block)){
+    	block[[person.x, person.y]] = 1;
+    }
 }
 
-var housesVisited = 0;
+//part 1
+for (var i = 0; i < input.length; i++) {
+	calcHouses(input[i], santa);
+};
+console.log(Object.keys(block).length);
+
+//part 2
+var block = {}; block[[0,0]] = 1;
+var santa = {x: 0, y:0} //x, y
+var robo = {x: 0, y:0} //x, y
 
 for (var i = 0; i < input.length; i++) {
-	calcHouse(input[i]);
+	calcHouses(input[i], (i%2 == 0) ? santa : robo);
 };
+
+console.log(Object.keys(block).length);
+
+
 
 
 
