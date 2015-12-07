6c62b8780b691d600d0f56e58dc0ad055b13cdee
zachatrocity <zachatrocity@gmail.com>
2015-12-07 10:53:25 -0600

    no message

--- aocday5.js
+++ aocday5.js
@@ -1,25 +1,36 @@
 var input = document.getElementsByTagName("pre")[0].innerHTML.split('\n');
 
-var String.prototype.containsAtLeastThreeVowels = function() {
-
+//part 1
+var vowels = function(str) {
+	return ((str.match(/([aeiou])/g) || []).length > 2)
 }	
-
-var String.prototype.containsDuplicateLetters = function(){
-
+var dupl = function(str){
+	return ((str.match(/(\w)\1+/g) || []).length > 0)
+}
+var noSubs = function(str){ //ab, cd, pq, or xy
+	return ((str.match(/^((?!ab|cd|pq|xy).)*$/g) || []).length == 1)
 }
 
-var String.prototype.doesNotContainCertainStrings = function(){ //ab, cd, pq, or xy
+//part 2
+var letBetween = function(str) {
+	return ((str.match(/(\w).\1/g) || []).length > 0)
+}	
 
+var duplPair = function(str){
+	return ((str.match(/(\w\w).*\1/g) || []).length > 0)
 }
 
 var result = 0;
+var resultTwo = 0;
 for (var i = 0; i < input.length - 1; i++) {
-	if(input[i].containsAtLeastThreeVowels() &&
-		input[i].containsDuplicateLetters() &&
-		input[i].doesNotContainCertainStrings()){
+	if(vowels(input[i]) && dupl(input[i]) && noSubs(input[i])){
 		result++;
+	}
+
+	if(letBetween(input[i]) && duplPair(input[i])){
+		resultTwo++;
 	}
 };
 
 console.log(result);
-
+console.log(resultTwo);
